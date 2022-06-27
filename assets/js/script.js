b69de628dc241e1c962b553c276ea440a02fc180
Noah Becker <102703954+noah35becker@users.noreply.github.com>
2022-06-26 21:06:18 -0400

    form can now collect data and create new html

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,15 +1,21 @@
 
 var formEl = document.querySelector('#task-form');
 var tasksToDoEl = document.querySelector('#tasks-to-do');
+var taskNameInput = document.querySelector('input[name="task-name"]');
+var taskTypeInput = document.querySelector('select[name="task-type"]');
 
 function createTaskHandler(event){
     event.preventDefault();
-    console.log(event);
 
     var listItemEl = document.createElement('li');
-    
     listItemEl.className = 'task-item';
-    listItemEl.textContent = 'This is a new task.';
+
+    var taskInfoEl = document.createElement('div');
+    taskInfoEl.className = 'task-info';
+    taskInfoEl.innerHTML = '<h3 class="task-name">' + taskNameInput.value + '</h3><span class="task-type">' + taskTypeInput.value + '</span>';
+    console.log(taskInfoEl.innerHTML);
+
+    listItemEl.appendChild(taskInfoEl);
 
     tasksToDoEl.appendChild(listItemEl);
 }
